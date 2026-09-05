fe9691d5fbc6de3a039bf92e9d7fcb042e293c0e
riyathakur8168 <riyathakur81143@gmail.com>
2026-09-05 11:53:06 +0530

    questions

--- morequestions-(PHASE _A) .py
+++ morequestions-(PHASE _A) .py
@@ -1,9 +1,9 @@
 #@ find the locatiopn and delete the element if exist 
 # arr = [10,20,30,40,50]
-# # target = 30
+# target = 30
 # target = 25
 # count = 0
-# for i in range(0,len(arr)-1):
+# for i in range(len(arr)):
 #     if arr[i] == target:
 #         arr.pop(i)
 #         count += 1
@@ -12,3 +12,26 @@
 #     print("target not found")
 # else:
 #     print("element is successfully delete fropm array and remian array :",arr)
+
+#@ remove duplicates from unsorted array
+# arr = [1,2,2,1,3,4,4,5,6,7,8,8,9,5]
+# res = []
+# seen = set()
+# for num in arr:
+#     if num not in seen:
+#         seen.add(num)
+#         res.append(num)
+# print(res)
+
+# find duplicates:ye ek ek karke duplicate find karega array mese
+# seen = set()
+# for num in arr:
+#     if num in seen:
+#         print(num)
+#     else:
+#         seen.add(num)
+
+# print(seen)
+
+
+
