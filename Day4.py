2e6764b29e01b4bfe8b931442ed1ef7d2b132826
riyathakur8168 <riyathakur81143@gmail.com>
2026-09-06 17:50:42 +0530

    day 4 practice

---
+++ Day4.py
@@ -0,0 +1,22 @@
+# Longest Subarray with Sum K
+arr = [10, 5, 2, 7, 1, 9]
+K = 15
+max_length = 0
+for i in range(len(arr)):
+    current_sum = 0
+    for j in range(i,len(arr)):
+        current_sum += arr[j]
+
+        if current_sum == K:
+            length = j-i+1
+            if length >max_length:
+                max_length = length
+                best_subarray = arr[i:j+1]
+print(max_length)
+print(best_subarray)
+
+
+
+
+
+#@ sliding window 
