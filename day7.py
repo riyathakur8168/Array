63da28f32fc689cc50d746e9a1a82c68ff46a962
riyathakur8168 <riyathakur81143@gmail.com>
2026-09-21 22:49:29 +0530

    more question practice

--- day7.py
+++ day7.py
@@ -18,7 +18,7 @@
 
 #-- longest subarray with sum less than or equal to k (only positive integers) 
 # #-- tc- o(n) and sc- o(1) 
-# nums = [2, 1, 3, 2, 4] # -- esme sirf positive integers honge 
+# nums = [2, 1, 3, 2, 4,3] # -- esme sirf positive integers honge 
 # k = 7
 # i = 0
 # j = 0
@@ -29,9 +29,9 @@
 # while j < len(nums):
 #     window_sum  += nums[j]   #-- window me sum add karhe hai 
 #     while window_sum > k:   #-- edar while k through esliy condition check karhe hai 
-#         window_sum -= nums[i] #- taki bar bar condition check kare taki eksath
-#         i += 1 #- khi baar shrink karna hoto hojaye ek bar karke na ruke 
-#     if j-i+1 > mx_length:   #-- edar if through check karhe hai ki mx_length update kare 
+#         window_sum -= nums[i]    ##- taki bar bar condition check kare taki eksath
+#         i += 1      ##- khi baar shrink karna hoto hojaye ek bar karke na ruke 
+#     if j-i+1 > mx_length:   ###-- edar if through check karhe hai ki mx_length update kare 
 #         mx_length = j-i+1
 #         start = i
 #         end = j
@@ -40,25 +40,45 @@
 # print(nums[start:end+1])
 
 #-- Minimum Size Subarray Sum
-#-- j → window ko bada karta hai
-#-- i → window ko chhota karta hai
-#@-- Jab tak window_sum < target hai tab tak expand karo
-# window_sum < target
+# Start
+#   ↓
+# i = 0, j = 0
+#   ↓
+# j se element window mein add karo
+#   ↓
+# sum calculate/update karo
+#   ↓
+# Kya sum >= 7 ?
+#   │
+#   ├── NO → j ko aage badhao → next element add karo
+#   │
+#   └── YES
 #         ↓
-#      expand
+#      Window VALID hai
 #         ↓
-#        j++
-
-#@--- jab window sum target se bda ya equal hoto answer check and save and shrink and again check
-# window_sum >= target
+#      Uski length nikalo
+#      j - i + 1
 #         ↓
-#    valid window
+#      Current minimum se compare karo
 #         ↓
-#  answer check
+#      Ab left se element remove karo
+#      i ko aage badhao
 #         ↓
-#      shrink
-#         ↓
-#        i++
+#      Kya new window ka sum >= 7 ?
+#         │
+#         ├── YES → ye bhi valid hai
+#         │          ↓
+#         │       length dobara check karo
+#         │          ↓
+#         │       phir shrink karo
+#         │
+#         └── NO → shrink karna STOP
+#                    ↓
+#                 j ko aage badhao
+#                    ↓
+#                 next element add karo
+#                    ↓
+#                   Repeat
 
 
 # target = 7
